05cd8434f49e97e5304c396a4fbfc92d5bd03c76
Pragash Sasitharan <Pragash1001@gmail.com>
2023-08-26 12:07:13 +0530

    Changes

--- BlogPage/assets/js/main.js
+++ BlogPage/assets/js/main.js
@@ -46,3 +46,20 @@ const swiper = new Swiper(".swiper", {
         }
     }   
 });
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
