506bcc9318838e0ba33717ebed3c92586e6ad61a
loshan20011 <loshan20011@gmail.com>
2023-08-27 23:19:54 +0530

    config

--- REGISTER/config.php
+++ REGISTER/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'FoodFlow');
+   define('DB_SERVER', 'foodflow-server.database.windows.net');
+   define('DB_USERNAME', 'Loshan');
+   define('DB_PASSWORD', 'L.oshxn_2001');
+   define('DB_DATABASE', 'foodflow-sql');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
