7f76959bf384a4f6d5d73b36ea931328fc87fd19
loshan20011 <loshan20011@gmail.com>
2023-08-28 22:21:10 +0530

    cart

--- ShopPage/foodie-master/assets/js/script.js
+++ ShopPage/foodie-master/assets/js/script.js
@@ -46,6 +46,7 @@ window.addEventListener("scroll", function () {
 
 
 
+
 /**
  * move cycle on scroll
  */
@@ -55,22 +56,21 @@ const deliveryBoy = document.querySelector("[data-delivery-boy]");
 let deliveryBoyMove = -80;
 let lastScrollPos = 0;
 
-window.addEventListener("scroll", function () {
+// window.addEventListener("scroll", function () {
 
-  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+//   let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
 
-  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-    let activeScrollPos = window.scrollY;
+//   if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+//     let activeScrollPos = window.scrollY;
 
-    if (lastScrollPos < activeScrollPos) {
-      deliveryBoyMove += 1;
-    } else {
-      deliveryBoyMove -= 1;
-    }
+//     if (lastScrollPos < activeScrollPos) {
+//       deliveryBoyMove += 1;
+//     } else {
+//       deliveryBoyMove -= 1;
+//     }
 
-    lastScrollPos = activeScrollPos;
-    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
-  }
+//     lastScrollPos = activeScrollPos;
+//     deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+//   }
 
-});
-
+// });
