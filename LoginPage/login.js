65a8692b0feff8e92ace0fb7acdad6f4fa056c91
loshan20011 <loshan20011@gmail.com>
2023-08-30 00:20:39 +0530

    backend-done

---
+++ LoginPage/login.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addUserForm = document.getElementById("login-form");
+
+    addUserForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(addUserForm);
+        const userData = Object.fromEntries(formData.entries());
+
+        const response = await fetch("http://localhost:1338/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        const result = await response.json();
+        console.log(result);
+    });
+})
