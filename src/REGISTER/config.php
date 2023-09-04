96351898e3752b5c8d2ffda9e5be9b9c151f7bbe
loshan20011 <loshan20011@gmail.com>
2023-09-05 00:31:46 +0530

    php-deleted

--- src/REGISTER/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'foodflow-server.database.windows.net');
-   define('DB_USERNAME', 'Loshan');
-   define('DB_PASSWORD', 'L.oshxn_2001');
-   define('DB_DATABASE', 'foodflow-sql');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
