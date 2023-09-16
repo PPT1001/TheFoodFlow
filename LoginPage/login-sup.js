703f18998e64852340d7e1b969eb2ece3513e024
Pragash Sasitharan <Pragash1001@gmail.com>
2023-09-16 15:05:15 +0530

    Changes

--- LoginPage/login-sup.js
+++ LoginPage/login-sup.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (result.status === "Login successful") {
             localStorage.setItem("is-loggedin-sup", true);
+            localStorage.setItem("is-loggedin-cus", false);
             window.location.href = "../SellerPage/index.html";
         }
     });
--- LoginPage/login.js
+++ LoginPage/login.js
@@ -22,10 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (result.status === "Login successful") {
             localStorage.setItem("is-loggedin-cus", true);
+            localStorage.setItem("is-loggedin-sup", false);
             window.location.href = "../ShopPage/foodie-master/index.html";
         }
-    });
-    
+    });   
 })
 
 
