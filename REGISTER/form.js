2f3c730f4f8a1607286be210be4b8a726b0ebd3e
ShakishnaviM <shakishnavi27@gmail.com>
2023-09-24 23:31:37 +0530

    changes

--- REGISTER/form.js
+++ REGISTER/form.js
@@ -33,6 +33,16 @@ document.addEventListener("DOMContentLoaded", () => {
             event.preventDefault();
 
         }
+        function showAlert(message) {
+            var customAlert = document.getElementById("customAlert");
+            var alertText = document.getElementById("alertText");
+
+            alertText.textContent = message;
+            customAlert.style.display = "block";
+        }
+
+        // Function to close the custom alert
+       
 
         function validaterec() {
             var Fname = document.recForm.name.value;
@@ -42,48 +52,48 @@ document.addEventListener("DOMContentLoaded", () => {
             var password = document.recForm.passwordHash.value;
             var cpassword = document.recForm.Cpassword.value;
             var filter1 = /[a-zA-Z]/g;
-            var filter2 = /[@$,<>#:?_*&;]/g;
+            var filter2 = /[@$,<>#:?_*&;!~%^]/g;
             var filter3 = /[0-9]/g;
             var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 
             if (Fname == null || Fname == "") {
-                alert("Full Name is required");
+                showAlert("Full Name is required");
                 return false;
             }
             else if (!isNaN(Fname)) {
-                alert("Enter only character in Full Name");
+                showAlert("Enter only character in Full Name");
                 return false;
             }
             else if (OrName == null || OrName == "") {
-                alert(" Orphanage/ Home name is required");
+                showAlert(" Orphanage/ Home name is required");
                 return false;
             }
             else if (!isNaN(OrName)) {
-                alert("Enter only character in Orphanage/ Home");
+                showAlert("Enter only character in Orphanage/ Home");
                 return false;
             }
 
             if (!Email.match(emailPattern)) {
-                alert("Enter a valid email address: " + Email);
+                showAlert("Enter a valid email address: " + Email);
                 return false;
             }
 
 
             else if (isNaN(Mnum) || Mnum == "" || Mnum == null || Mnum.length != 10) {
-                alert("Enter a valid Mobile Number");
+                showAlert("Enter a valid Mobile Number");
                 return false;
             }
             else if (password == null || password == "") {
-                alert("Password is required");
+                showAlert("Password is required");
                 return false;
             }
             else if (!(password.match(filter1) && password.match(filter2) && password.match(filter1) && password.match(filter3))) {
-                alert("Password must contain atleast one uppercase, one lowercase, one number and one special character");
+                showAlert("Password must contain atleast one uppercase, one lowercase, one number and one special character");
                 return false;
             }
             else if (password != cpassword || cpassword == null || cpassword == "") {
-                alert("confirm password does not match");
+                showAlert("confirm password does not match");
                 return false;
             }
             else {
